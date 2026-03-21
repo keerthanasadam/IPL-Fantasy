d9cae7c7694d672e8c3cc2abff525a74955212e0
keerthanasadam <keerthana.sadam@gmail.com>
2026-03-21 17:05:22 -0400

    fix: update start.sh to use fixed port and modify nginx configuration to listen on dynamic PORT

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 timeout 60 alembic upgrade head || echo 'Alembic timed out or failed, skipping'
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
